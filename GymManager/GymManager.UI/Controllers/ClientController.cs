e0ad2fcd93944ecc7ff8fbf5b9b0c7d023ff4160
Damian <damian.radomski9908@gmail.com>
2022-11-25 11:51:27 +0100

    Add client controller

---
+++ GymManager/GymManager.UI/Controllers/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.UI.Controllers
+{
+    public class ClientController : BaseController
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
